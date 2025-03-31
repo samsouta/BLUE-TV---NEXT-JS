5b9e86eaccfc9121441fdafb49846ccf910ce138
samsouta <samxsouta@gmail.com>
2025-03-31 22:59:11 +0800

    fix bu

---
+++ src/components/features/Home/hell.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const hell = () => {
+  return (
+    <div>
+        good
+    </div>
+  )
+}
--- src/components/features/Home/me.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const me = () => {
-  return (
-    <div>me</div>
-  )
-}
